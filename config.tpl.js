0a763f2560d51564f31ac60354a026bea04d3a0e
Sebastian O <juan.sebastian.oviedo@correounivalle.edu.co>
2025-09-14 14:29:53 -0500

    add secret with dockerhub

---
+++ config.tpl.js
@@ -0,0 +1,7 @@
+window.CONFIG = {
+  title: "$Treasura Hunt Sebas",
+  student: "Sebastian Oviedo 2510104",
+  salt: "DS3univalle",
+  keyHash: "e104d7f24d764dc3621cf3268dd81b4fd08900722fe14e2af64dd952913c9950",
+  clue: "SALCHIPAPA"
+};
